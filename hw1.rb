6bf330c5569c177d270438afe803d8782071589a
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2015-10-08 23:42:31 +0300

    Added a new line before return statement and cleaned up case statement

--- hw1.rb
+++ hw1.rb
@@ -1,12 +1,12 @@
 def convert_to_bgn(price, currency)
-  case
-    when currency == :bgn
+  case currency
+    when :bgn
       price.round(2)
-    when currency == :usd
+    when :usd
       (price * 1.7408).round(2)
-    when currency == :eur
+    when :eur
       (price * 1.9557).round(2)
-    when currency == :gbp
+    when :gbp
       (price * 2.6415).round(2)
   end
 end
@@ -14,5 +14,6 @@ end
 def compare_prices(first_price, first_currency, second_price, second_currency)
   first_price_bgn = convert_to_bgn(first_price, first_currency)
   second_price_bgn = convert_to_bgn(second_price, second_currency)
+
   first_price_bgn <=> second_price_bgn
 end
