add17aeb09e96f52cca7630867ee1b45abde1a6f
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2015-11-22 22:35:30 +0200

    Rearanged code and fixed some bugs

--- tasks/05/solution.rb
+++ tasks/05/solution.rb
@@ -32,24 +32,22 @@ class ObjectStore
     end
 
     def error?
-      # TODO: fix skeptic
-      ! @success
+      not @success
     end
   end
 
   class Commit
-    attr_reader :date, :message, :hash, :named_objects
+    attr_reader :date, :message, :hash, :stage
 
-    def initialize(date, message, hash, named_objects)
+    def initialize(date, message, hash, stage)
       @date = date
       @message = message
       @hash = hash
-      @named_objects = named_objects
+      @stage = stage
     end
 
     def objects
-      # TODO: check if you can remove the if
-      @named_objects.values if @named_objects
+      @stage.values
     end
   end
 
@@ -57,21 +55,6 @@ class ObjectStore
     @stage[name] = object
     @objects_changed += 1
     Outcome.new("Added #{name} to stage.", true, object)
-  end
-
-  def commit(message)
-    if @objects_changed == 0
-      Outcome.new("Nothing to commit, working directory clean.", false)
-    else
-      time = Time.now
-      time_string = time.strftime(TIME_FORMAT)
-      hash = Digest::SHA1.hexdigest(time_string + message)
-      new_commit = Commit.new(time, message, hash, @stage)
-      @store[@current_branch] << new_commit
-      outcome_message = "#{message}\n\t#{@objects_changed} objects changed"
-      @objects_changed = 0
-      Outcome.new(outcome_message, true, new_commit)
-    end
   end
 
   def remove(name)
@@ -85,12 +68,38 @@ class ObjectStore
     end
   end
 
+  def commit(message)
+    if @objects_changed == 0
+      Outcome.new("Nothing to commit, working directory clean.", false)
+    else
+      time = Time.now
+      time_string = time.strftime(TIME_FORMAT)
+      hash = Digest::SHA1.hexdigest(time_string + message)
+      new_commit = Commit.new(time, message, hash, @stage.dup)
+      @store[@current_branch] << new_commit
+      outcome_message = "#{message}\n\t#{@objects_changed} objects changed"
+      @objects_changed = 0
+      Outcome.new(outcome_message, true, new_commit)
+    end
+  end
+
+  def get(name)
+    last_commit = @store[@current_branch].last
+    if last_commit and last_commit.stage.has_key?(name)
+      Outcome.new("Found object #{name}.",
+                  true,
+                  @store[@current_branch].last.stage[name])
+    else
+      Outcome.new("Object #{name} is not committed.", false)
+    end
+  end
+
   def checkout(commit_hash)
     commit_index = @store[@current_branch].
       find_index { |commit| commit.hash == commit_hash }
     if commit_index
       @store[@current_branch].slice!((commit_index + 1)..-1)
-      @stage = @store[@current_branch].last.named_objects
+      @stage = @store[@current_branch].last.stage
       Outcome.new("HEAD is now at #{@store[@current_branch].last.hash}.",
                   true,
                   @store[@current_branch].last)
@@ -99,13 +108,36 @@ class ObjectStore
     end
   end
 
+  def head
+    if @store[@current_branch].empty?
+      Outcome.new("Branch #{@current_branch} does not have any commits yet.",
+                  false)
+    else
+      Outcome.new(@store[@current_branch].last.message,
+                  true,
+                  @store[@current_branch].last)
+    end
+  end
+
+  def log
+    if @store[@current_branch].empty?
+      Outcome.new("Branch #{@current_branch} does not have any commits yet.",
+                  false)
+    else
+      separate_messages = @store[@current_branch].reverse_each.map do |commit|
+        "Commit #{commit.hash}\n" +
+          "Date: #{commit.date.strftime(TIME_FORMAT)}\n\n" +
+            "\t#{commit.message}"
+      end
+      Outcome.new(separate_messages.join("\n\n"), true)
+    end
+  end
+
   class Branch
     def initialize(repository)
       @repository = repository
     end
 
-    # TODO: consider using instance_eval/class_eval instead of writing
-    #       @repository every time
     def create(branch_name)
       if @repository.store.has_key?(branch_name.to_sym)
         Outcome.new("Branch #{branch_name} already exists.", false)
@@ -113,21 +145,6 @@ class ObjectStore
         @repository.store[branch_name.to_sym] =
           @repository.store[@repository.current_branch].dup
         Outcome.new("Created branch #{branch_name}.", true)
-      end
-    end
-
-    def checkout(branch_name)
-      if @repository.store.has_key?(branch_name.to_sym)
-        @repository.current_branch = branch_name.to_sym
-        checkout_branch = @repository.store[@repository.current_branch]
-        if checkout_branch.empty?
-          @repository.stage = {}
-        else
-          @repository.stage = checkout_branch.last.named_objects
-        end
-        Outcome.new("Switched to branch #{branch_name}.", true)
-      else
-        Outcome.new("Branch #{branch_name} does not exist.", false)
       end
     end
 
@@ -144,6 +161,21 @@ class ObjectStore
       end
     end
 
+    def checkout(branch_name)
+      if @repository.store.has_key?(branch_name.to_sym)
+        @repository.current_branch = branch_name.to_sym
+        checkout_branch = @repository.store[@repository.current_branch]
+        if checkout_branch.empty?
+          @repository.stage = {}
+        else
+          @repository.stage = checkout_branch.last.stage
+        end
+        Outcome.new("Switched to branch #{branch_name}.", true)
+      else
+        Outcome.new("Branch #{branch_name} does not exist.", false)
+      end
+    end
+
     def list
       sorted_keys_with_prefix = @repository.store.keys.sort.map do |branch_name|
         ((branch_name == @repository.current_branch) ? "* " : "  ") +
@@ -155,42 +187,5 @@ class ObjectStore
 
   def branch()
     Branch.new(self)
-  end
-
-  def log
-    if @store[@current_branch].empty?
-      Outcome.new("Branch #{@current_branch} does not have any commits yet.",
-                  false)
-    else
-
-      separate_messages = @store[@current_branch].reverse_each.map do |commit|
-        "Commit #{commit.hash}\n" +
-          "Date: #{commit.date.strftime(TIME_FORMAT)}\n\n" +
-            "\t#{commit.message}"
-      end
-      Outcome.new(separate_messages.join("\n\n"), true)
-    end
-  end
-
-  def head
-    if @store[@current_branch].empty?
-      Outcome.new("Branch #{@current_branch} does not have any commits yet.",
-                  false)
-    else
-      Outcome.new(@store[@current_branch].last.message,
-                  true,
-                  @store[@current_branch].last)
-    end
-  end
-
-  def get(name)
-    last_commit = @store[@current_branch].last
-    if last_commit and last_commit.named_objects.has_key?(name)
-      Outcome.new("Found object #{name}.",
-                  true,
-                  @store[@current_branch].last.named_objects[name])
-    else
-      Outcome.new("Object #{name} is not committed.", false)
-    end
   end
 end
